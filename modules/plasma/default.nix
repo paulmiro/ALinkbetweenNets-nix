31ca64421bfddbbbccbfe5f0891493fdb1d8a711
paulmiro <30203227+paulmiro@users.noreply.github.com>
2025-03-27 16:18:59 +0100

    add aerial ssdm theme

--- modules/plasma/default.nix
+++ modules/plasma/default.nix
@@ -8,14 +8,14 @@ in {
       displayManager.sddm = {
         enable = true;
         # wayland.enable = true;
-        # theme = "/home/l/aerial-sddm-theme/";
-        # theme= "breeze";
+        theme = "aerial";
       };
       desktopManager.plasma6.enable = true;
     };
     environment.systemPackages = with pkgs; [
       kdePackages.sddm-kcm # sddm config module
       kdePackages.plasma-nm
+      (pkgs.callPackage ../../pkgs/aerial-sddm-theme { })
     ];
   };
 }
---
+++ pkgs/aerial-sddm-theme/default.nix
@@ -0,0 +1,32 @@
+{ lib, pkgs, ... }:
+pkgs.stdenv.mkDerivation {
+  pname = "aerial-sddm-theme";
+  version = "484b52e-2025-03-27";
+  src = pkgs.fetchFromGitHub {
+    owner = "3ximus";
+    repo = "aerial-sddm-theme";
+    rev = "484b52e1f26c29cb9825e23b3f3c9b8bef0bcf5f";
+    hash = "sha256-YeJTdlnGV58MNB8VpjDrFuC3VNsh5SgTdZH63aLD6Xw=";
+  };
+  buildInputs = with pkgs;  [
+    gst_all_1.gst-libav
+    gst_all_1.gst-plugins-good
+    libsForQt5.phonon-backend-gstreamer
+    libsForQt5.qt5.qtquickcontrols
+    libsForQt5.qt5.qtgraphicaleffects
+    libsForQt5.qt5.qtmultimedia
+  ];
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/share/ssdm/themes/aerial
+    cp -a * $out/share/ssdm/themes/aerial/
+    runHook postInstall
+  '';
+  meta = with lib; {
+    description = "Aerial theme for SDDM";
+    homepage = "https://github.com/3ximus/aerial-sddm-theme";
+    platforms = platforms.linux;
+    license = licenses.gpl3Only;
+    maintainers = with maintainers; [ link ];
+  };
+}
